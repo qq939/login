bc7301ee411f0c8550cd09b34017d1a0ae64aa9d
qq939 <939342547@qq.com>
2026-01-12 13:18:15 +0800

    1

--- createTable.py
+++ createTable.py
@@ -1,9 +1,8 @@
-from models import app, db
+from models import *
 
-def create_tables():
-    with app.app_context():
-        db.create_all()
-        print("All tables created successfully.")
+
 
 if __name__ == "__main__":
     create_tables()
+    create_user("jiang", "jiang")
+
--- models.py
+++ models.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 import zlib
+import dotenv
+import os
+# Load environment variables from .env file
+dotenv.load_dotenv()
+
 
 
 class Base(DeclarativeBase):
@@ -13,7 +18,7 @@ db = SQLAlchemy(model_class=Base)
 SHARD_COUNT = 2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{os.getenv("MYSQL_PASSWORD")}@dimond.top:3333/login'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -32,7 +37,7 @@ for i in range(SHARD_COUNT):
     table_name = f'user_{i}'
     
     # Create the class dynamically
-    model = type(
+    globals()[class_name] = type(
         class_name,
         (db.Model, UserMixin),
         {
@@ -40,19 +45,27 @@ for i in range(SHARD_COUNT):
             '__bind_key__': None # Use default bind
         }
     )
-    user_models[i] = model
+    user_models[i] = globals()[class_name]
 
 def get_user_model_by_username(username):
     shard_id = zlib.crc32(username.encode('utf-8')) % SHARD_COUNT
     return user_models[shard_id]
 
 def create_user(username, password):
-    Model = get_user_model_by_username(username)
-    new_user = Model(username=username, password=password)
-    db.session.add(new_user)
-    db.session.commit()
+    with app.app_context():
+        Model = get_user_model_by_username(username)
+        new_user = Model(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
     return new_user
 
 def get_user_by_username(username):
-    Model = get_user_model_by_username(username)
+    with app.app_context():
+        Model = get_user_model_by_username(username)
     return db.session.execute(db.select(Model).filter_by(username=username)).scalar_one_or_none()
+
+def create_tables():
+    with app.app_context():
+        db.create_all(bind_key=None)
+        print("All tables created successfully.")
+
